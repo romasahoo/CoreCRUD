a9679a4aca1090bcaa920c0601fd9669146d1df1
manas-sahu <manas.sahu.one@gmail.com>
2021-03-12 11:17:23 +0530

    Add project files.

---
+++ WebAPI/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Department
+    {
+        public int DeptId { get; set; }
+
+        public string DeptName { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ WebAPI/Models/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
